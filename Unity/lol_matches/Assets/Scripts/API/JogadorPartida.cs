6cfcf32a0e4ea971728b6f2e914fc14eaa3619d6
palhariniandre <warder63@gmail.com>
2024-12-14 16:24:09 -0300

    integracao jogador partida

--- Unity/lol_matches/Assets/Scripts/API/JogadorPartida.cs
+++ Unity/lol_matches/Assets/Scripts/API/JogadorPartida.cs
@@ -10,5 +10,17 @@ public class JogadorPartida
     public int farm;
     public int ouroAdquirido;
     public string campeao;
+    public int item1id;
+    public string item1nome;
+    public int item2id;
+    public string item2nome;
+    public int item3id;
+    public string item3nome;
+    public int item4id;
+    public string item4nome;
+    public int item5id;
+    public string item5nome;
+    public int item6id;
+    public string item6nome;
     
 }
