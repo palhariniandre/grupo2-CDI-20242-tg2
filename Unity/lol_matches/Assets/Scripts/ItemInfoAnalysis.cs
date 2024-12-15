dd4f5a64a83a3387fe3e2b21ed9f0c640e1aeadb
mortarialex <alex.pereira@ecomp.ufsm.br>
2024-12-15 17:30:18 -0300

    tela de item e campeao

---
+++ Unity/lol_matches/Assets/Scripts/ItemInfoAnalysis.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ItemInfoAnalysis : MonoBehaviour
+{
+    [Header("Item Info")]
+    [SerializeField] private TextMeshProUGUI itemName;
+    [SerializeField] private TextMeshProUGUI itemDescription;
+}
